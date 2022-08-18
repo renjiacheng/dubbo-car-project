7d875b3ee1998ad8507025c84622fdfce032388e
rengjiacheng <bookofshady@163.com>
2022-08-18 21:12:11 +0800

    first commit

---
+++ dubbo-car-project-employee-service-impl/src/test/java/com/rjc/employee/service/impl/MyTest.java
@@ -0,0 +1,12 @@
+package com.rjc.employee.service.impl;
+
+/**
+ * @author rjc
+ * @create 2022-08-18 21:10
+ */
+public class MyTest {
+
+    public static void main(String[] args) {
+        System.out.println("测试数据123");
+    }
+}
