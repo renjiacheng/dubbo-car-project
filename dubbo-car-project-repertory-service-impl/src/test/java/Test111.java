e0ceb5934c2deedd18d7e7686cca2ee69bdc55fd
litiecheng <litiechengstart@163.com>
2022-08-18 20:00:02 +0800

    ltc测试提交1

---
+++ dubbo-car-project-repertory-service-impl/src/test/java/Test111.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * @author ltcstart
+ * @date 2022/8/18 - 19:55
+ */
+public class Test111 {
+    @Test
+    public void test111(){
+        System.out.println("123123");
+    }
+}
