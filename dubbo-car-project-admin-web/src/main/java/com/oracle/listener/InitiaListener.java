f78273088f560b4ae21a274598cb8068840f72fa
litiecheng <litiechengstart@163.com>
2022-08-23 11:26:18 +0800

    加入监听器

---
+++ dubbo-car-project-admin-web/src/main/java/com/oracle/listener/InitiaListener.java
@@ -0,0 +1,35 @@
+package com.oracle.listener;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.ServletContextAware;
+
+import javax.servlet.ServletContext;
+
+/**
+ * @author ltcstart
+ * @date 2022/7/21 - 16:03
+ */
+@Component
+public class InitiaListener implements ServletContextAware, ApplicationListener<ContextRefreshedEvent> {
+
+//    ContextRefreshedEvent:上下文更新事件
+    private ServletContext servletContext;
+
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+            servletContext.setAttribute("root",servletContext.getContextPath());
+            servletContext.setAttribute("css",servletContext.getContextPath()+"/resources/css");
+            servletContext.setAttribute("image",servletContext.getContextPath()+"/resources/images");
+            servletContext.setAttribute("js",servletContext.getContextPath()+"/resources/js");
+            servletContext.setAttribute("kindeditor",servletContext.getContextPath()+"/resources/kindeditor");
+        }
+
+
+    @Override
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext=servletContext;
+    }
+}
